6af86036cb9d53fa3002a2a3689002746942f0dd
Mario <mario.espada77@gmail.com>
2023-01-07 17:24:02 +0100

    GenerateRandomString comment

--- pkg/generator/random_string.go
+++ pkg/generator/random_string.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// GenerateRandomString generate a random string given a lenght
 func GenerateRandomString(length int) string {
 	var letters = []rune("testuserndjdismaluehsmsldjd8a6egenidhnalsoduenkdoshsyabdlieee")
 	rand.Seed(time.Now().UTC().UnixNano())
